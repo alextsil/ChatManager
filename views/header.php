49b921a7510c1d0e3e399f6ec371e9cd3e02139d
alextsil <alex19924@gmail.com>
2013-12-16 12:28:53 +0200

    Created a mock header(top menu) for the website.

---
+++ views/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+    <head>
+        <title>Live chat</title>
+        <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+        <link type="text/css" rel="stylesheet" href="style/globalStyle.css" />
+    </head>
+    <body>
+    	<div id="topBar">
+    		<ul id="headerMenu">
+    			<li><h1><a href="../index.php">TroLLChat</a></h1></li>
+    			<li><a href="../index.php">Profile</a></li>
+    			<li><a href="../index.php">Bookmarks</a></li>
+    			<li><a href="../index.php">Contact Admin</a></li>
+    			<li><a href="../index.php">Logout</a></li>
+    		</ul>
+    		<ul id="headerStats">
+    			<li>Total Online:100</li>
+    		</ul>
+    	</div>
+    
+    
+    
+    
+    </body>
+</html>
