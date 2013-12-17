b727fffdbbdacc0986e1f28e405e5e3b973db40a
alextsil <alex19924@gmail.com>
2013-12-17 22:13:58 +0200

    Dimiourgisa to kentriko chat room box me mia mock sizitisi

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	require 'views/header.php';
 	require 'views/leftBar.php';
 	require 'views/rightBar.php';
+	require 'views/content.php';
 ?>
 	
 	
---
+++ views/content.php
@@ -0,0 +1,26 @@
+<link type="text/css" rel="stylesheet" href="views/style/content.css" />
+<div id="content">
+
+<!-- Ta li sketa simenoun message. to h2 channel name -->
+<ul id="chatBox">  
+	<li><h2>Global</h2></li>
+	<li><span class="user">Pambos</span> : Pame rakadiko?</li>
+	<li><span class="user">Polina92</span> : Nai!!!!!!!!!!!!!!!!!!!!!! <3</li>
+	<li><span class="user">Giwrgis</span> : Mesa paidiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.</li>
+	<li><span class="user">Pambos</span> : Xxxxxxx <3</li>
+	<li><span class="user">Polina92</span> : Pame rakadiko?</li>
+	<li><span class="user">Giwrgis</span> : Nai!!!!!!!!!!!!!!!!!!!!!! <3</li>
+	<li><span class="user">Pambos</span> : Mesa paidiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.</li>
+	<li><span class="user">Polina92</span> : Xxxxxxx <3</li>
+	<li><span class="user">Pambos</span> : Pame rakadiko?</li>
+	<li><span class="user">Polina92</span> : Nai!!!!!!!!!!!!!!!!!!!!!! <3</li>
+	<li><span class="user">Giwrgis</span> : Mesa paidiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.</li>
+	<li><span class="user">Pambos</span> : Xxxxxxx <3</li>
+	<form action="index.php" method="post" id="messageForm">
+    <input type="text" name="message" id="userTextBox">
+    <p>Press enter to send!</p>
+</form>
+</ul>
+
+
+</div>
